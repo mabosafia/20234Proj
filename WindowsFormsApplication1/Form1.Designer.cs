e305031b8add7d8ab7d4d123e0dbd2491344adc5
mabosafia@gmail.com <mabosafia@gmail.com>
2023-05-03 00:05:54 +0300

    sssdsadasddasd

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(134, 82);
+            this.button1.Location = new System.Drawing.Point(152, 129);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
